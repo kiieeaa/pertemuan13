7f7a122947f6d53d80558b2157fa2dd1c82c2cb9
kiieeaa <kiyaadinda5@gmail.com>
2025-06-10 18:18:37 +0700

    selesai dalam menulis sekarang tutup filenya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -21,7 +21,9 @@ int main() {
         //loop akan berhenti jika anda memasukan karakter q
         if (baris == "q") break;
         //menulis dan memasukan nilai dari 'baris' ke dalam file
-
+        outfile << baris << endl;
 
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
 }
