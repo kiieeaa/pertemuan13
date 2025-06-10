2da48649ac47712428576dfb433bf3807ffed6c0
kiieeaa <kiyaadinda5@gmail.com>
2025-06-10 18:14:12 +0700

    unlimited loop untuk menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -11,4 +11,7 @@ int main() {
     // membuka file dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= menuis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
 }
