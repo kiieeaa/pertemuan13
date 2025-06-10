e6033f934cfd835af62dd87e10239d9c2c7fabaa
kiieeaa <kiyaadinda5@gmail.com>
2025-06-10 18:16:11 +0700

    loop akan berhenti jika anda memasukan karakter q

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -17,6 +17,8 @@ int main() {
     while (true) {
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
 
 
     }
